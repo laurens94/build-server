b77416c29890ea8561cb53481d9e9f96b162f856
Anoesj Sadraee <anoesjsadraee@gmail.com>
2015-03-16 18:31:58 -0400

    color coding and checking if gruntfile, gemfile or packages.json exist

--- index.js
+++ index.js
@@ -1,60 +1,97 @@
 'use strict';
 
+/* WEBHOOK ADDRESS: http://128.199.56.106:3000/github */
+
 var express = require('express'),
-    debug = require('debug')('api'),
-    bodyParser = require('body-parser'),
-    Githook = require('git-hook'),
-    shell = require('shelljs'),
-    fs = require('fs');
+  debug = require('debug')('api'),
+  bodyParser = require('body-parser'),
+  Githook = require('git-hook'),
+  shell = require('shelljs'),
+  fs = require('fs'),
+  colors = require('colors');
 
 var app = express();
-var gh = new Githook();
+var gh = new Githook({
+  github: {
+
+  }
+});
 
 app.use(bodyParser.json({
-    limit: '1mb'
+  limit: '1mb'
 }));
 
 app.get('/', function (req, res) {
-    res.status(200).send('yo world');
+  res.status(200).send('hee niet spieken droplul');
 });
 
 app.post('/github', function (req, res) {
-    debug('github event');
-    gh.handleEvent('github', {
-        ip: '192.30.252.1', // gh.determineIP(req),
-        headers: req.headers,
-        body: req.body
-    }, function (err) {
-        if (err) {
-            res.status(400).send('Event not supported');
-        } else {
-            if (req.body.ref == 'refs/heads/master') {
-                res.status(200).send('Building your shizzle.');
-                res.end();
+  console.log(colors.bold('Incoming GitHub event!'));
+  debug('github event');
 
-                var folderName = req.body.repository.name
-                console.log('Starting with: ' + folderName)
+  gh.handleEvent('github', {
+    ip: '192.30.252.41',
+    headers: req.headers,
+    body: req.body
+  }, function (err) {
+    if (err) {
+      res.status(400).send('Event not supported');
+    } else {
+      if (req.body.ref == 'refs/heads/master') {
+        res.status(200).send('Building your shizzle.');
+        res.end();
 
-                if (!fs.existsSync('sites/' + folderName)){
-                    console.log('Git clone: ' + folderName)
-                    shell.exec('cd sites; git clone git@github.com:' + req.body.repository.full_name + '.git');
-                }
+        var sitesFolder = 'sites/';
+        var folderName = req.body.repository.name;
+        console.log(colors.yellow('Starting with: %s'), folderName);
 
-                console.log('npm install')
-                shell.exec('cd sites/' + folderName + '; npm install');
-
-                console.log('bundle install')
-                shell.exec('cd sites/' + folderName + '; bundle install');
-
-                console.log('git pull')
-                shell.exec('cd sites/' + folderName + '; git pull origin master');
-
-                console.log('grunt deploy')
-                shell.exec('cd sites/' + folderName + '; grunt deploy');
-            }
+        if (!fs.existsSync(sitesFolder + folderName)){
+          console.log(colors.yellow('git clone git@github.com: %s.git'), req.body.repository.full_name);
+          shell.exec('cd '+ sitesFolder + '; git clone git@github.com:' + req.body.repository.full_name + '.git');
+          console.log(colors.green('Done cloning %s'), folderName);
         }
-    });
+
+        console.log(colors.yellow('git pull origin master'));
+        shell.exec('cd '+ sitesFolder + folderName + '; git pull origin master');
+        console.log(colors.green('Done pulling the latest changes from GitHub.'));
+
+        // packages.json check
+        if (fs.existsSync(sitesFolder + folderName + '/packages.json')) {
+          console.log(colors.green('packages.json found'));
+          console.log(colors.yellow('npm install'));
+          shell.exec('cd '+ sitesFolder + folderName + '; npm install');
+          console.log(colors.green('Done installing packages.'), folderName);
+        } else {
+          console.log(colors.blue('no packages.json found'));
+        }
+
+        // GEMFILE check
+        if (fs.existsSync(sitesFolder + folderName + '/GEMFILE')) {
+          console.log(colors.green('GEMFILE found'));
+          console.log(colors.yellow('bundle install'));
+          shell.exec('cd '+ sitesFolder + folderName + '; bundle install');
+          console.log(colors.green('Done installing gems.'), folderName);
+        } else {
+          console.log(colors.blue('no GEMFILE found'));
+        }
+
+        // Gruntfile check
+        if (fs.existsSync(sitesFolder + folderName + '/Gruntfile.js')) {
+          console.log(colors.green('Gruntfile.js found'));
+          console.log(colors.yellow('grunt deploy'));
+          shell.exec('cd '+ sitesFolder + folderName + '; grunt deploy');
+          console.log(colors.green('Succesfully deployed project!'), folderName);
+        } else {
+          console.log(colors.blue('no Gruntfile.js found'));
+        }
+
+        console.log(colors.green('Done.\n'));
+        console.log(colors.yellow('Waiting for incoming GitHub events...\n'));
+      }
+    }
+  });
 });
 
 app.listen(3000);
-console.log('started server on port 3000');
+console.log(colors.green('Started server on port 3000.'));
+console.log(colors.yellow('Waiting for incoming GitHub events...\n'));
