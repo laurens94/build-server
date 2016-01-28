85dc5462d9b43fee2e317176f272e3cc2d340c81
Daniel Beeke <daniel@studiofonkel.nl>
2016-01-28 12:46:50 +0100

    up2

--- builder.js
+++ builder.js
@@ -234,7 +234,7 @@ var builder = {
                     cname = params.commit.branch + '.' + cname;
                 }
 
-                vhost = vhost.replace('[REPLACE_WITH_CNAME]', cname + ' ' + cname + builderDomain);
+                vhost = vhost.replace('[REPLACE_WITH_CNAME]', cname + ' ' + cname + '.' + builderDomain);
 
                 try {
                     fs.writeFile(builder.getVhostPath(params.commit), vhost);
