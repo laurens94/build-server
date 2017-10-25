3d0ab02d82dd99aeadf64b6b65f2f2346385b69e
Laurens <laurens.bruijn@gmail.com>
2017-10-25 14:30:56 +0200

    test

--- index.js
+++ index.js
@@ -38,6 +38,9 @@ function getSignature (buf) {
 function verifyRequest (req, res, buf, encoding) {
     var expected = req.headers['x-hub-signature'];
     var calculated = getSignature(buf);
+    if (expected) {
+        console.log ('Expected present');
+    }
     if (expected !== calculated) {
         throw new Error('Invalid signature.');
     }
